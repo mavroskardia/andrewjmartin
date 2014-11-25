057aed9d368c0eb89b6cb733a916cd44954d110d
mavroskardia <chryso@gmail.com>
2014-11-25 17:40:37 -0500

    Added more TV-like styling

--- ajm/landing/static/landing/js/main.js
+++ ajm/landing/static/landing/js/main.js
@@ -20,14 +20,14 @@
 
  	var typlay = new Typlay('typlay_container', msg.join('\n'));
 
- 	typlay.minspeed = 100;
- 	typlay.maxspeed = 300;
+ 	typlay.minspeed = 10;
+ 	typlay.maxspeed = 30;
 
  	typlay.onend = function () {
  		Utilities.remove_class('navlinks', 'hidden').remove_class('typlay_container', 'cursor-shown'); 		
  		var name_elt = Utilities.getElementByInnerText('Andrew Martin');
  		name_elt.innerHTML = name_elt.innerHTML.replace('Andrew Martin', '<a href="#">Andrew Martin</a>');
- 		Utilities.add_class('typlay_interlace', 'fadeout');
+ 		Utilities.add_class('typlay_interlace', 'fadeout').add_class('typlay_cabinet', 'fadeout');
  	}
 
  	window.setTimeout(function () {

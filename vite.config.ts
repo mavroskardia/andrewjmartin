e1f3cf223d83e062ea2ff2e2f837d389a2892f41
Andy Martin <andrew.martin@infotechinc.com>
2023-04-11 00:57:17 -0400

    Configuring for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
+      entry: 'index.html',
       formats: ['es'],
     },
     rollupOptions: {
